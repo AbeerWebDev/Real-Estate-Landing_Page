cff68b2cba68bbf41ceead82203a2e9afba889a6
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2023-06-20 12:06:11 +0400

    Navbar update

--- myapp/src/components/Navbar/Navbar.jsx
+++ myapp/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import './Navbar.scss'
 const Navbar = () => {
+  const [active, setActive] = useState(false)
+  const [open, setOpen] = useState(false)
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false)
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener('scroll', isActive)
+    }
+  })
   return (
-    <div className='navbar'>
-        <div className="container">
-            <div className="logo">
-                <span>ALH PROPERTIES</span>
-            </div>
-            <div className="links">
-                <span>About</span>
-                <span>Developer</span>
-                <span>Location</span>
-                <button>BROCHURE</button>
-            </div>
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <span className="text">ALH PROPERTIES</span>
         </div>
+        <div className="links">
+          <button onClick={()=> setOpen(!open)}>BROCHURE
+          {open && <div className="options">
+            
+          </div>
+          }
+          </button>
+        </div>
+      </div>
+      { active && (
+        <>
+        <hr />
+        <div className="menu">
+          <span>About</span>
+          <span>Developer</span>
+          <span>Location</span>
+        </div>
+      </>
+      )}
     </div>
-  )
+  );
 }
 
 export default Navbar
